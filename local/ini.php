864b9dd10f5897c8ef79e5330b7d9f6e1f0c5c3a
Fraggle1180 <Fraggle@mail.ru>
2020-01-06 13:27:29 +0300

    Update ini.php

--- local/ini.php
+++ local/ini.php
@@ -1,1 +1,13 @@
-<?
+$settings = array(
+	'db::connection::host'		=> 'localhost',
+	'db::connection::user'		=> '',
+	'db::connection::password'	=> '',
+	'db::connection::database'	=> '',
+	'db::connection::allow_empty_password'	=> true,
+
+	'sms::apikeys::smspilot'	=> '',
+
+	'mail::mailchimp::apikey'	=> '',
+
+	'mail::sendgrid::apikey'	=> '',
+);
